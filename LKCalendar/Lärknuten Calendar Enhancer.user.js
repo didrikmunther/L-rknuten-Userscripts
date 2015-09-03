73a2441ad588cb687653b9da2105b378df4dbe56
Malaxiz <malaxiz.main@gmail.com>
2015-09-03 12:35:28 +0200

    Changed version number

--- LKCalendar/Lärknuten Calendar Enhancer.user.js
+++ LKCalendar/Lärknuten Calendar Enhancer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Lärknuten Calendar Enhancer
 // @namespace    http://github.com/Malaxiz
-// @version      3.3
+// @version      3.3.1
 // @updateURL    https://github.com/Malaxiz/L-rknuten-Userscripts/raw/master/LKCalendar/L%C3%A4rknuten%20Calendar%20Enhancer.user.js
 // @description  Enhances the calendar
 // @icon		 https://i.imgur.com/Xa4Svs9.png
